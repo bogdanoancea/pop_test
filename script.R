dff18f5b46f004ef79eb6db5a60914c2e4fc4b98
Bogdan Oancea <Bogdan.Oancea@insse.ro>
2018-05-04 13:31:56 +0300

    script.R

--- script.R
+++ script.R
@@ -325,3 +325,89 @@ system.time({
 })
 
 
+
+p = vector(length = 24)
+for(i in 1:24) {
+  p[i] = which(N_0$NIDT[i] == rownames(trans_N1N0))
+}
+x<-trans_N1N0[p,p]
+
+x[1,]<-14
+
+nMNOmat = x
+#trans_N1N0
+nReg = N_0$pop_official
+
+u0 <- rowSums(nMNOmat) /nReg
+cv_u0 <- 0.15
+fu <- lapply(u0, function(u){
+  umin <- max(0, u - cv_u0 * u)
+  umax <- min(1, u + cv_u0 * u)
+  output <- list('unif', xMin = umin, xMax = umax)
+  return(output)
+})
+
+# List of priors for v
+v0 <- nReg /100
+cv_v0 <- 0.10
+fv <- lapply(v0, function(u){
+  umin <- max(0, u - cv_v0 * u)
+  umax <- u + cv_v0 * u
+  output <- list('unif', xMin = umin, xMax = umax)
+  return(output)
+})
+
+# List of priors for lambda
+cv_lambda <- 0.6
+alpha <- 1 / cv_lambda**2 - 1
+flambda <- lapply(v0, function(v){list('gamma', shape = 1 + alpha, scale =  v / alpha)})
+
+# Names and parameters of priors for the transition probabilities
+distNames <- rep('unif', 24)
+variation <- rep(list(list(cv = 0.20)), 24)
+
+system.time({
+  post_N1 <- postNt(nMNOmat/100,nReg/100, fu, fv, flambda, distNames, variation, scale = 100)
+})
+
+p = vector(length = 24)
+for(i in 1:24) {
+  p[i] = which(N_0$NIDT[i] == rownames(trans_N1N0))
+}
+for(i in 1:40) {
+
+  x<-transitions_all[[i]][p,p]
+  x[1,]<-14
+  nMNOmat = x
+  nReg = N_0$pop_official
+  u0 <- rowSums(nMNOmat) /nReg
+  cv_u0 <- 0.15
+  fu <- lapply(u0, function(u){
+    umin <- max(0, u - cv_u0 * u)
+    umax <- min(1, u + cv_u0 * u)
+    output <- list('unif', xMin = umin, xMax = umax)
+    return(output)
+  })
+
+  # List of priors for v
+  v0 <- nReg /100
+  cv_v0 <- 0.10
+  fv <- lapply(v0, function(u){
+    umin <- max(0, u - cv_v0 * u)
+    umax <- u + cv_v0 * u
+    output <- list('unif', xMin = umin, xMax = umax)
+    return(output)
+  })
+
+  # List of priors for lambda
+  cv_lambda <- 0.6
+  alpha <- 1 / cv_lambda**2 - 1
+  flambda <- lapply(v0, function(v){list('gamma', shape = 1 + alpha, scale =  v / alpha)})
+
+  # Names and parameters of priors for the transition probabilities
+  distNames <- rep('unif', 24)
+  variation <- rep(list(list(cv = 0.20)), 24)
+  print(i)
+  y<-postNt(nMNOmat/100,nReg/100, fu, fv, flambda, distNames, variation, scale = 100)
+  print(y)
+}
